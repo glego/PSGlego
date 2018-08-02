9caa11698c4457ea9674b52563a17d4906466abf
Glenn Goffin <glenn.goffin@gmail.com>
2018-08-02 05:41:34 +0200

    Add PowerShell ISE MS bug 1803...

---
+++ Playground/PowershellISE-Preview/Install.ps1
@@ -0,0 +1,8 @@
+
+# Install PowerShellISE Preview for current user
+#
+# Solution for PowerShell ISE issue on Windows 10 1803 17134.1
+# Link: https://social.technet.microsoft.com/Forums/en-US/47ded9fb-0501-4953-92cc-ef55dbf86fbe/powershell-ise-where-are-the-default-snippets-after-win10-upgrade?forum=winserverpowershell
+
+Install-Module PowerShellISE-preview -Scope CurrentUser
+isep
